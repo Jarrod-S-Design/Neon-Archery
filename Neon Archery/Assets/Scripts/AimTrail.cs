f5dd969f61ed74baeafd3101db7b42ba8d419577
Jarrod Sevior <jarrod.sevior@gmail.com>
2018-05-29 12:38:52 +1000

    no message

---
+++ Neon Archery/Assets/Scripts/AimTrail.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AimTrail : MonoBehaviour 
+{
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () 
+	{
+		if (Input.GetMouseButtonDown(0))
+		{
+			this.GetComponent<TrailRenderer> ().enabled = true;
+		}
+		if (Input.GetMouseButtonUp(0))
+		{
+			this.GetComponent<TrailRenderer> ().enabled = false;
+		}
+		this.transform.position = Input.mousePosition;
+	}
+}
---
+++ Neon Archery/Assets/Scripts/Shooting.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour 
+{
+	public Transform bulletSpawn;
+	public GameObject bulletPrefab;
+	public float bulletSpeed;
+
+	//Shooting Timer
+	public float waitTime;
+
+	private bool canShoot = true;
+	public float timePassed;
+
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () 
+	{
+		//var trigger = XCI.GetButton(XboxButton.A);
+		timePassed += Time.deltaTime;
+
+		if (Input.GetButton("Submit")) 
+		{
+			if (canShoot == true && timePassed >= waitTime) 
+			{
+				FireBullet ();
+			}
+		}
+
+		if ((timePassed >= waitTime) && (canShoot == false))
+		{
+			timePassed = 0;
+			canShoot = true;
+		}
+	}
+
+	void FireBullet ()
+	{
+		GameObject GO = Instantiate (bulletPrefab, bulletSpawn.position, Quaternion.identity) as GameObject;
+		GO.GetComponent<Rigidbody> ().AddForce (this.transform.up * bulletSpeed, ForceMode.Impulse);
+		canShoot = false;
+	}
+}
