0da6350f6c3174e1a05f01444fc7e0dd6fb83857
Jarrod Sevior <jarrod.sevior@gmail.com>
2018-06-12 14:23:31 +1000

    --- Added --- - iTween Unity Package - Set up base scriptable and foundations for target patterns - Scene for testing target patterns

---
+++ Neon Archery 2D/Assets/Scripts/ScriptablePath.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Path", menuName = "Path")]
+public class ScriptablePath : ScriptableObject 
+{
+	public GameObject pathToFollow;
+	public string pathName;
+	public float speed;
+	public float waveSize;
+}
---
+++ Neon Archery 2D/Assets/Scripts/TargetPath.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetPath : MonoBehaviour 
+{
+	public GameObject pathToFollow;
+	[SerializeField] private string pathName;
+	private float speed;
+	private float waveSize;
+	// Use this for initialization
+	void Start () 
+	{
+		// Assign scriptable variables
+
+		iTween.Init (gameObject);
+		iTween.MoveTo (gameObject, iTween.Hash ("path", iTweenPath.GetPath (pathName), "time", 2, "easetype",iTween.EaseType.easeInOutSine));
+	}
+
+	// Update is called once per frame
+	void Update () 
+	{
+
+//		iTween.MoveAdd (target, targetPath[1], 2);
+
+	}
+
+//	void OnDrawGizmos()
+//	{
+//		iTween.DrawPath (targetPath, Color.red);
+//	}
+}
